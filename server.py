6b1f83f4797669c5142f0ddbcaa5c99fb7d28186
orizada <89981079+orizada@users.noreply.github.com>
2023-01-07 10:51:13 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ LENGTH, HEIGHT = 600,600
 LENS = pygame.Rect(LENGTH/2-50,HEIGHT/2-50,100,100)
 CLIENTS = {}
 CLOCK, FPS = pygame.time.Clock(), 60
+SRV_SOCKET = socket.socket()
 
 pygame.init()
 pygame.display.set_caption("PICTURE")
@@ -33,33 +34,64 @@ class Client:
                 self.matrix = pickle.loads(data[1])
 
     def ask_rect(self,pos,size):
-        message = f"REQ|{pos[0]}|{pos[1]}|{size[0]}|{size[1]}###"
-        self.sock.send(message.encode())
-
+        message = f"REQ|{pos[0]}|{pos[1]}|{size[0]}|{size[1]}###ID={self.id}"
+        print(message)
+        #self.sock.send(message.encode())
     def draw(self):
         for y,row in enumerate(self.matrix):
             for x,value in enumerate(row):
-                SCREEN.fill(value,((x,y),(1,1)))
+                # 1 is left-top, 2 is right-top, 3 is left-down, 4 is right-down
+                #if even adds half-length to x
+                # need to describe about y
+                SCREEN.fill(value,(((LENGTH/2)*((self.id+1)%2)+x,(HEIGHT/2)*((self.id+1)/3)+y),(1,1)))
     def exit(self):
+        global CLIENTS
         self.thread.join()
+        del CLIENTS[self.id]
 
 def get_lens():
-    pass
+    if LENS.left < LENGTH/2:
+        if LENS.y<HEIGHT/2: # ID=1
+            x_pos,y_pos = LENS.x,LENS.y
+            length = min(LENS.width,LENGTH/2-x_pos)
+            height = min(LENS.height,HEIGHT/2-y_pos)
+            #CLIENTS[1].ask_rect((x_pos,y_pos),(length,height))
+        if LENS.bottom>HEIGHT/2: # ID = 3
+            x_pos,y_pos = LENS.x,max(LENS.y,HEIGHT/2)
+            length = min(LENS.width,LENGTH/2-x_pos)
+            height = LENS.bottom-y_pos
+            #CLIENTS[3].ask_rect((x_pos,y_pos),(length,height))
+    if LENS.right > LENGTH/2:
+        if LENS.y<HEIGHT/2: # ID=2
+            x_pos,y_pos = max(LENS.x,LENGTH/2),LENS.y
+            length = min(LENS.width,LENGTH/2-x_pos)
+            height = min(LENS.height,HEIGHT/2-y_pos)
+            #CLIENTS[2].ask_rect((x_pos,y_pos),(length,height))
+            print((x_pos,y_pos),(length,height))
+        if LENS.bottom>HEIGHT/2: # ID = 4
+            x_pos,y_pos = LENS.x,max(LENS.y,HEIGHT/2)
+            length = min(LENS.width,LENGTH/2-x_pos)
+            height = LENS.bottom-y_pos
+            #CLIENTS[4].ask_rect((x_pos,y_pos),(length,height))
+            #print((x_pos,y_pos),(length,height)) 
 
 
 def accept_connections():
-    global CLIENTS
-    srv_socket = socket.socket()
-    srv_socket.bind(("0.0.0.0",8168))
-    srv_socket.listen(4)
+    global CLIENTS, SRV_SOCKET
+    SRV_SOCKET = socket.socket()
+    SRV_SOCKET.bind(("0.0.0.0",8168))
+    SRV_SOCKET.listen(4)
     threads = []
     while RUNNING:
         if len(CLIENTS)<4:
-            sock, address = srv_socket.accept()
-            client = Client(len(CLIENTS)+1,sock)
-            CLIENTS[client.id]+=1
+            try:
+                sock, address = SRV_SOCKET.accept()
+                client = Client(len(CLIENTS)+1,sock)
+                CLIENTS[client.id]+=1
+            except Exception as e:
+                print(f"ERROR!! {e}")
 def graphics():
-    global LENS
+    global LENS, RUNNING
     movement = [0,0,2]
     while RUNNING:
         CLOCK.tick(FPS)
@@ -69,10 +101,11 @@ def graphics():
         LENS.y += movement[1]*movement[2]
         pygame.draw.rect(SCREEN,(255,255,255),LENS,1)
         pygame.display.flip()
-        print(movement)
+        get_lens()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                exit()
+                RUNNING = False
+                SRV_SOCKET.close()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_DOWN:
                     movement[1] =+ 1
